cdf715a3d8c38bcbf2701306555507c09e3d067a
chenjiancong <Email>
2020-05-12 22:14:57 +0800

    add a file

---
+++ request/request.js
@@ -0,0 +1,14 @@
+//封装网络请求数据
+export const request = (params) => {
+    return new Promise((resolve, reject) => {
+        wx.request({
+            ...params,
+            success: function (res) {
+                resolve(res)
+            },
+            fail: function () {
+                reject(err)
+            },
+        })
+    })
+}
